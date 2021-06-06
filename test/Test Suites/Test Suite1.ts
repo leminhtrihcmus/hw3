7621f4fe234a55068933e6aa03ecde1002e9307d
baokhanh13 <baokhanh1306nsc@gmail.com>
2021-06-07 01:37:13 +0700

    Add test

---
+++ test/Test Suites/Test Suite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b173de53-a6b3-443b-a98a-37bfc006b659</testSuiteGuid>
+</TestSuiteEntity>
