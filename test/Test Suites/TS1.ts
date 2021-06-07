e3c30d63d76fd55dc74cdc67ea555be19a8873ad
baokhanh13 <baokhanh1306nsc@gmail.com>
2021-06-08 01:32:30 +0700

    Fix

--- test/Test Suites/TS1.ts
+++ test/Test Suites/TS1.ts
@@ -17,4 +17,22 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/TC1</testCaseId>
    </testCaseLink>
+   <testCaseLink>
+      <guid>2ce1d675-57e7-4619-8f11-e22189c8c77c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9ee91f81-2156-4939-911b-40d2b35886df</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ec5783c1-e793-4114-8cfc-500aaf9853f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC4</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
