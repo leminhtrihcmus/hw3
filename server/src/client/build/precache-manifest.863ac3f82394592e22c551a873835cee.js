7ecc650b0fc0a48a1588a0832743046a172726ae
baokhanh13 <baokhanh1306nsc@gmail.com>
2021-06-07 23:57:19 +0700

    Fix

---
+++ server/src/client/build/precache-manifest.863ac3f82394592e22c551a873835cee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e741cdf488b7af3dcc20461f2e3325",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d5efb1628e9501fd9a5",
+    "url": "/static/js/2.0f2a3cbf.chunk.js"
+  },
+  {
+    "revision": "df00f11a5b19bd29377d265c843f9b9f",
+    "url": "/static/js/2.0f2a3cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522b85c86f2370c5ef1b",
+    "url": "/static/js/main.0cc5d59f.chunk.js"
+  },
+  {
+    "revision": "f860306cbc49e59d4329",
+    "url": "/static/js/runtime-main.6c97de84.js"
+  },
+  {
+    "revision": "ed8dd0aa7b6ce38bae7cf59622ebb6a4",
+    "url": "/static/media/big-spaceman.1b378c1b.ed8dd0aa.png"
+  },
+  {
+    "revision": "de158994bb10e755504f13fcf722b83e",
+    "url": "/static/media/crossword.de158994.png"
+  }
+]);
